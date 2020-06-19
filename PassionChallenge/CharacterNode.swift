a4bbe9c4bb91ef28657e8c1e415234391f3b0af9
Beatrix Lee <blsr3@academy.cin.ufpe.br>
2020-06-19 18:11:37 -0300

    comenta stateMachine

--- PassionChallenge/CharacterNode.swift
+++ PassionChallenge/CharacterNode.swift
@@ -15,17 +15,23 @@ class CharacterNode: SKSpriteNode{
     var left = false
     var right = false
     
+    var jump = false
+    var landed = false
+    var grounded = false
+    
+    
     var hSpeed: CGFloat = 0.0
     
     var walkSpeed: CGFloat = 2
     
-    var stateMachine: GKComponent?
+    //    var stateMachine: GKComponent?
+    //
+    //
+    //    func setUpStateMachine(){
+    //        let normalState = NormalState(with: self)
+    //        stateMachine = GKStateMachine(states: [normalState])
+    //        stateMachine?.enter(NormalState.self)
+    //    }
+    //}
     
-    
-    func setUpStateMachine(){
-        let normalState = NormalState(with: self)
-        stateMachine = GKStateMachine(states: [normalState])
-        stateMachine?.enter(NormalState.self)
-    }
 }
-
