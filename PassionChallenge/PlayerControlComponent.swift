950f1dcc7b408d4c8267925f128ce664419e3db7
Beatrix Lee <blsr3@academy.cin.ufpe.br>
2020-06-19 18:07:02 -0300

    cria setup de controles, associando eles a cena

--- PassionChallenge/PlayerControlComponent.swift
+++ PassionChallenge/PlayerControlComponent.swift
@@ -9,25 +9,25 @@
 import SpriteKit
 import GameplayKit
 
-class PlayerControlComponent: GKComponent, ControlInputDelegate {
-
-    var touchControlNode : TouchControlInputNode?
-
+class PlayerControlComponent: GKComponent, ControlnputDelegate {
+    
+    var touchControlNode : TouchControlInputNode? = nil
+    
     func setupControls (camera: SKCameraNode, scene: SKScene){
-
+        
         touchControlNode = TouchControlInputNode(frame: scene.frame)
-        touchControlNode?.inputDelegate = self
+        touchControlNode?.InputDelegate = self
         touchControlNode?.position = CGPoint.zero
-
-
+        
+        
         camera.addChild(touchControlNode!)
-
+        
     }
-
+    
     func follow(command: String?){
-
+        
         print("command: \(String(describing: command))")
     }
-
-
+    
+    
 }
